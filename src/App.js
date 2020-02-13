fd774eda49c5734ef4ccfaa1f48913ac8a253d0e
Joseph Mills <millsj1077@gmail.com>
2020-02-13 16:16:40 -0600

    Does more stuff, needs styling real bad.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Home from "./pages/home/Home";
 import Donate from "./pages/donate/Donate";
 import Contact from "./pages/contact/Contact";
 import NavBar from "./components/NavBar";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import monaDark from "./assets/GitHub-Mark-120px-plus.png";
 //import "App.scss";
 
 export default function App() {
   return (
     <Router>
       <div className="App-Container">
+        <a href="https://github.com/" target="_blank">
+          <img src={monaDark} alt="<Mona isn't showing up>" />
+        </a>
+        <header>some stuff</header>
         <NavBar name="NavBar" />
         <Switch>
           <Route path="/donate" component={Donate} />
--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,5 +1,65 @@
 import React from "react";
 
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <>
+      <img src="" alt="<image of Dupe>" />
+      <h1>Dupe's Contact Page Header</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Id interdum velit
+        laoreet id donec. Nisl suscipit adipiscing bibendum est ultricies.
+        Venenatis cras sed felis eget velit aliquet. Vulputate dignissim
+        suspendisse in est ante in nibh mauris cursus. Quisque non tellus orci
+        ac auctor augue mauris. Nibh tellus molestie nunc non. Pretium aenean
+        pharetra magna ac placerat. Nunc eget lorem dolor sed. Purus in mollis
+        nunc sed id semper risus in.
+      </p>
+
+      <p>
+        Urna nunc id cursus metus aliquam. Nibh mauris cursus mattis molestie a
+        iaculis. Vitae sapien pellentesque habitant morbi tristique. Ut ornare
+        lectus sit amet est placerat in egestas erat. Semper risus in hendrerit
+        gravida rutrum quisque non. Convallis a cras semper auctor neque vitae
+        tempus. Tincidunt vitae semper quis lectus nulla at volutpat. Ut
+        porttitor leo a diam sollicitudin tempor id eu nisl. Cursus risus at
+        ultrices mi tempus. Risus viverra adipiscing at in tellus integer
+        feugiat scelerisque varius. Interdum varius sit amet mattis vulputate.
+        Aliquam ut porttitor leo a diam sollicitudin tempor. Venenatis a
+        condimentum vitae sapien pellentesque habitant. Sociis natoque penatibus
+        et magnis dis parturient. Volutpat lacus laoreet non curabitur gravida.
+        Egestas dui id ornare arcu odio ut. Eget nulla facilisi etiam dignissim.
+        Duis at tellus at urna condimentum mattis pellentesque. Aenean vel elit
+        scelerisque mauris pellentesque. Tristique senectus et netus et. Ut
+        tortor pretium viverra suspendisse potenti nullam ac. Felis donec et
+        odio pellentesque diam volutpat commodo sed egestas. Eu turpis egestas
+        pretium aenean pharetra. Nec feugiat nisl pretium fusce id velit ut
+        tortor. Sit amet consectetur adipiscing elit duis. Quis commodo odio
+        aenean sed adipiscing diam donec adipiscing tristique. Felis eget nunc
+        lobortis mattis aliquam faucibus purus in. A erat nam at lectus urna
+        duis convallis convallis. Et malesuada fames ac turpis egestas sed
+        tempus urna. Mi bibendum neque egestas congue quisque egestas diam in
+        arcu. Sed nisi lacus sed viverra tellus in. Urna nec tincidunt praesent
+        semper. Venenatis tellus in metus vulputate eu. Maecenas ultricies mi
+        eget mauris pharetra et ultrices neque. Risus ultricies tristique nulla
+        aliquet enim tortor. Facilisis sed odio morbi quis commodo. Vestibulum
+        sed arcu non odio. Sit amet nisl suscipit adipiscing bibendum est
+        ultricies. Donec adipiscing tristique risus nec feugiat. Quis eleifend
+        quam adipiscing vitae. Viverra ipsum nunc aliquet bibendum enim
+        facilisis gravida neque. Maecenas pharetra convallis posuere morbi leo
+        urna molestie. Adipiscing at in tellus integer. Sit amet nisl suscipit
+        adipiscing bibendum est ultricies integer quis. Elementum integer enim
+        neque volutpat ac tincidunt vitae. Ornare arcu odio ut sem nulla
+        pharetra diam sit amet. Maecenas ultricies mi eget mauris pharetra et.
+        In iaculis nunc sed augue lacus viverra vitae congue eu. Sit amet
+        porttitor eget dolor morbi. Sed odio morbi quis commodo odio aenean.
+        Duis tristique sollicitudin nibh sit amet commodo nulla. Urna nec
+        tincidunt praesent semper feugiat nibh sed. Aliquam sem fringilla ut
+        morbi tincidunt augue. Bibendum neque egestas congue quisque egestas
+        diam. At tempor commodo ullamcorper a lacus vestibulum sed arcu non.
+        Massa id neque aliquam vestibulum morbi. Id eu nisl nunc mi ipsum
+        faucibus vitae aliquet nec. Nunc sed velit dignissim sodales ut eu sem.
+      </p>
+    </>
+  );
 }
